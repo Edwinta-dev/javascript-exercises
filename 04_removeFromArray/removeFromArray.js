ee800a84b4eef607f57e863df55983f263891c16
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-22 17:14:27 +0800

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...removeElement) {
+    for(let a=0;a<removeElement.length;a++){
+        /*for(let i=0;i<array.length;i++){
+            if (removeElement[a]===array[i]){
+                array.splice(i,1);
+            }
+        }*/
+       let i=0;
+       while (i<array.length){
+            if (removeElement[a]===array[i]){
+                array.splice(i,1);
+            }
+            else{
+                i++;
+            }
+       }
+    } 
+    return array;
 };
 
 // Do not edit below this line
