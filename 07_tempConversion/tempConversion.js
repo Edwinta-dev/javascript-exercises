f983ade8f3ff7569e4576b83d9d3e46af48651cf
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-22 18:08:07 +0800

    Completed Exercis 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+     let result=((farenheitTemp-32)*(5/9));
+     return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+    let result=((celsiusTemp*(9/5))+32);
+    return parseFloat(result.toFixed(1));
 };
+/*
+From celsius to farenheit
+TEMP celsius= (TEMP *(9/5)+32) farenheit
+TEMP farenheit = ((TEMP -32)*(5/9)) celsius
 
+
+
+
+*/
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
