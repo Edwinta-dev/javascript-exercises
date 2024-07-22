e2bef7ca62a1f387fa5a87f46617cf411240327b
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-22 16:37:04 +0800

    Completed exercise 01 to 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(userInput,numRepeat) {
+    if (numRepeat<0){
+        return "ERROR";
+    }else if(numRepeat===0){
+        return "";
+    }else{
+        /* Using a js method
+        return (userInput.repeat(numRepeat));
+        */
+       // Using for loops to achieve
+       let result = "";
+       for (let a=0;a<numRepeat;a++){
+            result+=userInput;
+       } 
+       return result;
+    }
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(originalStr) {
+    let reverseString = "";
+    for (let i = originalStr.length - 1 ; i > -1; i--){
+        reverseString+=originalStr.charAt(i);
+    }
+    return reverseString;
 
 };
 
