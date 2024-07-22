d4d0f43f5786f68b75a5e5b0a7b07ba2364ce8b4
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-22 17:41:08 +0800

    Completed Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sumFloor;
+    let sumCeiling;
+    if ((num1>num2)&&(num1>0)&&(num2>0)&&(Number.isInteger(num1))&&(Number.isInteger(num2))){
+        sumFloor=num2;
+        sumCeiling=num1;
+    }else if ((num2>num1)&&(num1>0)&&(num2>0)&&(Number.isInteger(num1))&&(Number.isInteger(num2))){
+        sumFloor=num1;
+        sumCeiling=num2;
+    }else {
+        return "ERROR";
+    }
+    let adder=sumFloor;
+    let result=sumFloor;
+    for(let i = sumFloor;i<sumCeiling;i++){
+        adder+=1;
+        result+=adder;
+    }
+    return result;
 };
 
 // Do not edit below this line
