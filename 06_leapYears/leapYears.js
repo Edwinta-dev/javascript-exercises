92605b7f7d3c23e0211a809089761f6342729e70
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-22 17:54:44 +0800

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year%4===0)){
+        if ((year%100!==0)||((year%100===0)&&(year%400===0))){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+/*To be a leap year, the year number must be divisible by four – except for end-of-century years, 
+which must be divisible by 400. This means that the year 2000 was a leap year, although 1900 was not*/
