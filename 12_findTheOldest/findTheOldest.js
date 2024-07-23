3fcaeeedf3a57ea424037de0e19a72b404a0afe0
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2024-07-23 13:05:11 +0800

    Completed 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let elementAge = 0;
+    let oldestPerson = [0,0];
+    for(counter=0;counter<people.length;counter++){
+        if(people[counter].yearOfDeath===undefined){
+            elementAge = 2024-people[counter].yearOfBirth;
+        }else{
+            elementAge = people[counter].yearOfDeath-people[counter].yearOfBirth;
+        }
+        oldestPerson = ageComparator(oldestPerson,elementAge,counter);
+    }
+    return people[oldestPerson[1]];    
 };
 
+
+function ageComparator(oldestPerson,elementAge,counter){
+    if (elementAge>oldestPerson[0]){
+        oldestPerson[0] =elementAge;
+        oldestPerson[1] = counter;
+    }
+    return oldestPerson;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
